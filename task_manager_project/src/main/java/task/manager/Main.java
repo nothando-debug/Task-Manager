c7b1dcf5daf3e661d638fbe50eb08a38321c736b
nothando-debug <nndlovu426@gmail.com>
2026-05-12 21:59:42 +0200

    feat: intiliazed the task manager project and create the Task class

---
+++ task_manager_project/src/main/java/task/manager/Main.java
@@ -0,0 +1,7 @@
+package task_manager_project.src.main.java.task.manager;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ task_manager_project/src/main/java/task/manager/Task.java
@@ -0,0 +1,11 @@
+package task_manager_project.src.main.java.task.manager;
+
+
+public class Task {
+    String list;
+    public Task(String list){
+        this.list = list;
+    }
+    
+    
+}
