c8ba4670fe7fadf8463055582a19d9497bdab399
nothando-debug <nndlovu426@gmail.com>
2026-05-13 21:45:42 +0200

    fix: added a few a comments on how the logic will work

--- task_manager_project/src/main/java/task/manager/TaskManager.java
+++ task_manager_project/src/main/java/task/manager/TaskManager.java
@@ -1,4 +1,8 @@
+package task_manager_project.src.main.java.task.manager;
+
 public class TaskManager {
     
-    
+
 }
+//should be do actual business logic of the class. 
+//implement methods like view, creat, edit , and delete
