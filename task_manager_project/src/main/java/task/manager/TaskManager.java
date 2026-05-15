1921d5046d82c7f69f645393c050a224b8a62c52
nothando-debug <nndlovu426@gmail.com>
2026-05-15 21:05:18 +0200

    feat: added viewTasks method

--- task_manager_project/src/main/java/task/manager/TaskManager.java
+++ task_manager_project/src/main/java/task/manager/TaskManager.java
@@ -1,6 +1,18 @@
 package task_manager_project.src.main.java.task.manager;
 
+import java.util.List;
+
 public class TaskManager {
+
+    public List<String> viewTasks():
+        return List<String>;
+
+    
+
+
+
+
+
     
 
 }
