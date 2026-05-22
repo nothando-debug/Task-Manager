a564f3fe06c532ba126c39430d456ec3bcebbd49
nothando-debug <nndlovu426@gmail.com>
2026-05-22 13:46:38 +0200

    added docs to help me understand the flow before I start with logic

--- task_manager_project/src/main/java/task/manager/CreateCommand.java
+++ task_manager_project/src/main/java/task/manager/CreateCommand.java
@@ -1,5 +1,10 @@
 package task.manager;
 
 public class CreateCommand {
-    
+    //responsible for creating a task. thats all
+    //when a user types "create" there should be a thing that pops and says 
+    //Enter your task: ... then the user enters the task and clicks when they are done.
+    //then a message should pop up and say "Your task has been added successfully!"
+    //then a user should be able to type help at anytime, when they want to remember
+    //what the commands.
 }
--- task_manager_project/src/main/java/task/manager/Task.java
+++ task_manager_project/src/main/java/task/manager/Task.java
@@ -13,6 +13,8 @@ public class Task {
     }
 
     
+
+    
     
     
     
